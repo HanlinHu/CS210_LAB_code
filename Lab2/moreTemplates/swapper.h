90bb3eae7a387ec3af8607c723e75a04647171d4
HanlinHu <HanlinHu@users.noreply.github.com>
2015-07-25 13:00:24 -0600

    Create swapper.h

---
+++ Lab2/moreTemplates/swapper.h
@@ -0,0 +1,16 @@
+//#ifndef  __SWAPPER_H__
+//#define  __SWAPPER_H__
+
+template<typename T>
+class my_class
+{
+	T x;
+	T y;
+public:
+	my_class(T a, T b);
+	void swapper();
+	void printem();
+};
+
+//#endif
+//#include "swapper.cpp"
